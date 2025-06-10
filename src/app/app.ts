5464524ffd57c9a03189f5f2e332349934a2df9b
shoaibalirts <shoaibali.rts@gmail.com>
2025-06-10 17:42:51 +0200

    imported tailwindcss

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserProfile } from './crud/pages/userprofile/user-profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,UserProfile],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'CRUD-App-Angular';
-  protected name = 'Shoaib Ali';
 }
---
+++ src/app/crud/pages/userprofile/user-profile.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'user-profile',
+    templateUrl: 'user-profile.html',
+    styleUrl: 'user-profile.css',
+})
+
+export class UserProfile{
+    protected userName="Shoaib Ali";
+} 
