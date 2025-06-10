8de9eb4f3b896e31784454ba2f050bbe3f198b73
shoaibalirts <shoaibali.rts@gmail.com>
2025-06-10 11:34:49 +0200

    overviewed angular app project structure, got starting point, made some changes on app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'crud-app-angular';
+  protected title = 'CRUD-App-Angular';
+  protected name = 'Shoaib Ali';
 }
